ae822699dd239deebb27b67890088331564af05b
ltann <leeroyleeroy18@gmail.com>
2019-02-19 23:01:30 -0800

    react todo with hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function Todo({ todo, index }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  )
 }
 
-export default App;
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(!value) return 
+    addTodo(value)
+    setValue("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        className="input" 
+        value={value} 
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)} 
+      />
+    </form>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      Complete: false
+    },
+    {
+      text: 'Walk the dog',
+      Complete: false
+    },
+    {
+      text: 'Go to the gym',
+      Complete: false
+    },
+  ])
+
+  const addTodo = text => {
+    const newTodo = [...todos, { text }]
+    setTodos(newTodo)
+  }
+
+  return(
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
+
+}
+
+export default App
